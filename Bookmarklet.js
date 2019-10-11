43a65fce8ee18919fe93664077529d2c65aac34c
archerymaster12354 <38110094+archerymaster12354@users.noreply.github.com>
2019-10-11 11:06:17 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,1 +1,1 @@
-window.open ('https://gitpod.io/#https://github.com/archerymaster12354/Blank', '_blank');
+javascript: window.open ('https://gitpod.io/#https://github.com/archerymaster12354/Blank', '_blank');
